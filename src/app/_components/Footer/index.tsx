5883bfd81274dfd7ef8abcc150d49a6b87017947
yoursaadahmad <mosesnuggz@gmail.com>
2024-08-11 01:57:07 +0500

    Final

---
+++ src/app/_components/Footer/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Link from 'next/link'
+
+import type { Footer } from '../../../payload/payload-types'
+import { fetchFooter } from '../../_api/fetchGlobals'
+import FooterComponent from './FooterComponent'
+
+export async function Footer() {
+  let footer: Footer | null = null
+
+  try {
+    footer = await fetchFooter()
+  } catch (error) {
+    console.log(error)
+  }
+
+  const navItems = footer?.navItems || []
+
+  return (
+    <>
+      <FooterComponent footer={footer} />
+    </>
+  )
+}
